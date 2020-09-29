d58d22a7c3a2f780b75ca145404e788949805450
yurianxdev <bryangarzon22@gmail.com>
2020-09-29 16:46:48 -0500

    Tailwind CSS Migration: PhoCard component Fixes #107

--- src/vue/components/ui/PhoCard.vue
+++ src/vue/components/ui/PhoCard.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
+  <div :class="{'hover:shadow-xl': isClickable}" class="rounded-lg p-5 shadow-lg transition-shadow duration-100">
+    <div v-if="logo" class="w-2/12 rounded-lg mb-2">
+      <img :src="logo" alt="logo" class="w-full">
+    </div>
     <slot />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    isClickable: {
+      type: Boolean,
+      default: false,
+    },
+    logo: {
+      type: String,
+      required: false,
+    },
+  },
+});
+</script>
